8fb7cff5228af51987ed8d9304de4dd84ef5f46f
Sharath <hsharath1@gmail.com>
2026-02-06 18:24:30 +0530

    start it from initial

--- src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/signup', authController.signup);
-router.post('/verify', authController.verify);
-
-module.exports = router;
